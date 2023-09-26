ec01b091fbbb176d2ebe42edd76127e1e191911c
Nelson Fernando <nelsonfernandoe@gmail.com>
2023-09-26 14:27:13 +0530

    Refactored index html file

---
+++ public/js/helper.js
@@ -0,0 +1,199 @@
+$('.select').timezones();
+
+$(document).ready(function () {
+    /* add initial tab data on page load */
+    addGroup();
+
+    $("#dynamicSetting,#close").on("click", function () {
+        $("#customDiv").toggle();
+    });
+
+});
+
+$(document).on('click', '.select', function () {
+    $('.select').timezones();
+});
+
+$(document).on('click', '.dynamicgroup', function (event) {
+    var getlength = $(this).attr('data-length');
+    console.log("length check", getlength);
+    if ($(this).attr('data-tab') != undefined) {
+        $(".dynamic-tabs1").css('display', 'none');
+        $("#" + $(this).attr('data-tab')).css('display', 'block');
+        $('.select').timezones();
+    }
+    $(".dynamic-tabs1").css('display', 'none');
+    $(".dynamic-tabs1").removeClass('active');
+    $(".dynamicgroup").removeClass('active');
+    $("#v-pills-dynamic" + getlength + "-tab").addClass('active');
+    $("#v-pills-dynamic" + getlength).addClass('show active');
+    $("#v-pills-dynamic" + getlength).css('display', 'block');
+});
+
+$(document).on('click', '#addGroup', function (event) {
+    addGroup();
+});
+
+
+$(document).on('click', '.removeGroup', function (event) {
+    var removeId = $(this).attr('data-id');
+    var lengthid = $(this).attr('data-length') - 1;
+
+    $("#" + removeId).remove();
+    $("#" + removeId + '-tab').remove();
+    $(".dynamic-tabs1").removeClass('active');
+    $(".dynamicgroup").removeClass('active');
+
+    $("#v-pills-dynamic" + lengthid + "-tab").addClass('active');
+    $("#v-pills-dynamic" + lengthid).addClass('show active');
+    $("#v-pills-dynamic" + lengthid).css('display', 'block');
+
+    configureRemoveGroupBtn();
+});
+
+function configureRemoveGroupBtn() {
+    const totalTabs = $('.removeGroup').length;
+    if (totalTabs <= 1)
+        $(".removeGroup").css('display', 'none');
+    else
+        $(".removeGroup").css('display', 'block');
+}
+
+function addGroup() {
+    var grouplength = $(".dynamicgroup").length + 1;
+
+    var addGroup = ' <button class="nav-link dynamicgroup" id="v-pills-dynamic' + grouplength + '-tab" data-tab="v-pills-dynamic' + grouplength + '" data-bs-toggle="pill" data-bs-target="#v-pills-dynamic' + grouplength + '" type="button" role="tab" aria-controls="v-pills-dynamic' + grouplength + '" data-length="' + grouplength + '" aria-selected="false">Dynamic Group' + grouplength + '</button>';
+
+    var addnewTab = '       <div role="tabpanel" id="v-pills-dynamic' + grouplength + '" aria-labelledby="v-pills-dynamic' + grouplength + '-tab" class="fade tab-pane dynamic-tabs1"> <form class="">' +
+        ' <div class="container">' +
+        '  <div class="row">' +
+        '<div class="col-lg-12 col-md col">' +
+        '  <div class="justify-content-md-left row">' +
+        '  <div class="col-lg-7 col">' +
+        '   <h5 style="font-size: 15px; text-align: left;">Dynamic Attribute' + grouplength + '</h5>' +
+        '  </div>' +
+        '  <div class="col-lg-5 col"></div>' +
+        '  </div>' +
+        ' <div class="row">' +
+        ' <div class="col-md-4">' +
+        ' <select aria-label="Dynamic Attribute" class="form-select attribute-select " style="font-size: 12px;">' +
+        '   <option>Dynamic Attribute</option>' +
+        '   <option value="2">2</option>' +
+        '   <option value="3">3</option>' +
+        '   <option value="4">4</option>' +
+        '  </select>' +
+        ' </div>' +
+        '  <div class="col-md-4">' +
+        ' <select aria-label="Dynamic Attribute" class="form-select " style="font-size: 12px;">' +
+        '  <option>Select Relationship</option>' +
+        '  <option value="2">equals</option>' +
+        '  <option value="3">greater </option>' +
+        '  <option value="4">greater than</option>' +
+        '  <option value="3">less </option>' +
+        '  <option value="4">less than</option>' +
+        '  </select>' +
+        '  </div>' +
+        '    <div class="col-md-4">' +
+
+        '  <div class="form-input">' +
+
+        '   <input type="text" class="form-control" id="usr">' +
+        '  </div>' +
+        ' </div>' +
+        ' </div>' +
+        ' </div>' +
+        '  <div class="col-lg-12 col-md col mt-3">' +
+        ' <div class="justify-content-md-left justify-content-lg-left row">' +
+        ' <div class="col-lg-7 col">' +
+        '  <h5 style="font-size: 15px; text-align: left;">Dynamic Attribute</h5>' +
+        ' </div>' +
+        '  <div class="col-lg-5 col"></div>' +
+        ' </div>' +
+        ' <div class="row ">' +
+        ' <div class="col"> <select aria-label="Dynamic Attribute" class="attibute-date form-select w-75" style="font-size: 12px;">' +
+        '    <option>Dynamic Attribute</option>' +
+        '   <option value="2">2</option>' +
+        '    <option value="3">3</option>' +
+        '    <option value="4">4</option>' +
+        '   </select>' +
+        ' </div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+
+        '<div class="mt-2 row">' +
+        '<div class="col-lg-2 col-md col">' +
+        '  <label class="form-label" style="font-size: 12px;">Duration</label>' +
+        '  <select aria-label="1" class="form-select" style="font-size: 12px;">' +
+        '   <option>1</option>' +
+        '  <option value="2">2</option>' +
+        '  <option value="3">3</option>' +
+        '  <option value="4">4</option>' +
+        ' </select>' +
+        ' </div>' +
+        '<div class="col-lg-5 col-md col">' +
+        '  <label class="form-label">&nbsp;</label>' +
+        '  <select aria-label="Days" class="form-select" style="font-size: 12px;">' +
+        '  <option>Days</option>' +
+        ' <option value="2">Weeks</option>' +
+        '  <option value="3">Months</option>' +
+
+        '   </select>' +
+        ' </div>' +
+        ' <div class="col-lg-5 col-md col">' +
+        '  <label class="form-label">&nbsp;</label>' +
+        '  <select aria-label="After" class="form-select" style="font-size: 12px;">' +
+        '   <option>Before</option>' +
+        '  <option value="2">After</option>' +
+        '    <option value="3">On</option>' +
+
+        '   </select>' +
+        '   </div>' +
+        ' </div>' +
+        '<div class="col-lg-12 col-md col mt-3">' +
+
+        ' <div class="row ">' +
+        '  <label class="form-label text-center" style="font-size: 12px;">Timezone</label>' +
+        '<div class="col"> <select name="timezone" id="timezone" aria-label="Dynamic Attribute" class="form-select select" ' + 'style="font-size: 12px;">' +
+        '  <option data-offset="+5:30" value="Asia/Calcutta">(GMT +5:30) Asia/Calcutta</option>' +
+        ' </select>' +
+        ' </div>' +
+
+        ' </div>' +
+        ' </div>' +
+        '<div class="mt-2 row">' +
+        ' <div class="col-lg-10 col-md col">' +
+        ' <div class="checkbox text-center" style="font-size: 12px;">' +
+        '  <input type="checkbox" value=""> Extend wait duration untill specific time' +
+        '</div>' +
+
+        ' </div>' +
+        ' </div>' +
+        ' <div class="row">' +
+        '<div class="col-lg-4 col-md col">' +
+        '<button  data-id="v-pills-dynamic' + grouplength + '" data-length="' + grouplength + '"  class="removeGroup btn btn-danger btn-sm" style="text-decoration: none; margin-top: 100px;">Remove group</button>' +
+        '</div>' +
+        /*'<div class="col-lg-4 col-md col">'+
+          '<button id="close" class="btn btn-warning btn-sm" style="text-decoration: none; margin-top: 100px;">Close</button>'+
+        '</div>'+*/
+
+        '<div class="col-lg-4 col-md col">' +
+        '<button id="done" class="btn btn-primary btn-sm" style="text-decoration: none; margin-top: 100px;">Done</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</form></div>';
+
+    // $('.select').timezones();
+    $(".dynamic-tabs1").css('display', 'none');
+    $("#v-pills-tab").append(addGroup);
+    $("#v-pills-tabContent").append(addnewTab);
+    $('#v-pills-dynamic' + grouplength).addClass('active');
+    $(".dynamic-tabs1").removeClass('active');
+    $(".dynamicgroup").removeClass('active');
+    $("#v-pills-dynamic" + grouplength + "-tab").addClass('active');
+    $("#v-pills-dynamic" + grouplength).addClass('show active');
+    $("#v-pills-dynamic" + grouplength).css('display', 'block');
+
+    configureRemoveGroupBtn();
+}
